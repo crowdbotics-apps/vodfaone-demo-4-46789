151d1ca8ce840ab608ea27b0a97446f27d929a9a
Crowdbotics <team@crowdbotics.com>
2024-03-04 16:18:33 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "vodfaone-demo-4-46789",
+  url: "https://vodfaone-demo-4-46789.botics.co",
+  api: "https://vodfaone-demo-4-46789.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
